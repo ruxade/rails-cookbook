14fba9724345b4322a8f32abe36ed1610e21840d
Adelina Gutu <w1705972@gmail.com>
2024-10-03 09:12:20 +0100

    wip

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -29,7 +29,7 @@ class CategoriesController < ApplicationController
   private
 
   def category_params
-    params.require(:category).permit(:name)
+    params.require(:category).permit(:name, :photo)
 
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :bookmarks
   has_many :categories, through: :bookmarks
+  # has_one_attached :photo
 
   validates :description, presence: true, uniqueness: true
   # validates :name, presence: true
